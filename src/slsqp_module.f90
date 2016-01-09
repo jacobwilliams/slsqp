408078d3ffed1c6dceadaf9a2cb801a00e06a04e
Jacob Williams <jacobwilliams@users.noreply.github.com>
2016-01-09 12:04:00 -0600

    added references and license.

--- src/slsqp_module.f90
+++ src/slsqp_module.f90
@@ -1,6 +1,22 @@
 !*******************************************************************************
-!>
-!  module for the SLSQP optimization method.
+!> license: BSD
+!
+!  Module containing the SLSQP optimization method.
+!
+!### References
+!  * [Original sourcecode at NETLIB](http://www.netlib.org/toms/733)
+!  * D. Kraft, "A software package for sequential quadratic programming",
+!    Technical Report DFVLR-FB 88-28, Institut fur Dynamik der Flugsysteme,
+!    Oberpfaffenhofen, July 1988.
+!  * D. Kraft, "[Algorithm 733: TOMP--Fortran modules for optimal control calculations](http://dl.acm.org/citation.cfm?id=192124),"
+!    ACM Transactions on Mathematical Software, Vol. 20, No. 3, p. 262-281 (1994).
+!
+!### License
+!  * Original sourcecode was released under a BSD license.
+!  * Modifications by Jacob Williams released under a similar BSD license.
+!
+!### History
+!  * Development continues on [GitHub](https://github.com/jacobwilliams/slsqp).
 
     module slsqp_module
 
